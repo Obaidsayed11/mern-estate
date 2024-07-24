6b5c07c8b547d13d80b0901958bc24b78f813167
Obaidsayed11 <sayedobaid699@gmail.com>
2024-07-24 15:18:18 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from  "c:/Users/Night Agent/Desktop/mern-estate/client/src/pages/SignUp"
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Signin from './pages/signin';
+
+
 
 export default function App() {
   return (
-    <h1 className='text-red-400'>App</h1>
+    <BrowserRouter>
+    <Routes>
+       <Route path="/" element={<Home />} />
+       <Route path="/signin" element={<Signin />} />
+       <Route path="/signup" element={<SignUp />} />
+       <Route path="/about" element={<About />} />
+       <Route path="/profile" element={<Profile />} />
+
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
 
