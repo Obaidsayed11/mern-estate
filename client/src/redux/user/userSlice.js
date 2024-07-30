c9a252ea147d2b6a4de5feb1d924aabc20e75492
Obaidsayed11 <sayedobaid699@gmail.com>
2024-07-30 19:31:45 +0530

    halfway signin

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    currentUser: null,
+    error: null,
+    loading: false,
+};
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        signInstart: (state) => {
+            state.loading = true;
+
+        },
+        signInSuccess:(state, action) => {
+    state.currentUser = action.payload;
+    state.loading = false;
+    state.error = null;
+        },
+        signInFailure : (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        }
+}
+   
+
+
+});
+ export const { signInstart, signInSuccess, signInFailure} = userSlice.action;
+
+ export default userSlice.reducer;
